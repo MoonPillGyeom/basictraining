d6a6864bd4be4874a8c0f13905edabf6b72862b5
MoonPillGyeom <ansvkf0820@naver.com>
2025-04-04 22:08:42 +0900

    refactor: Date를 구하는 중복 코드 제거

--- caleander/index.js
+++ caleander/index.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  getCurrentWeek();
-  getCurrentMonthInDays();
   const prevBtn = document.getElementById("prevBtn");
   const nextBtn = document.getElementById("nextBtn");
   const calendarDatas = document.getElementById("calendarDatas");
@@ -10,34 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const currentYear = today.getFullYear();
   const currentDay = today.getDay();
 
-  const dayOfWeek = document.getElementById("dayOfWeek");
-
-  const currentDate = document.getElementById("currentDate");
-  currentDate.innerText = getCurrentDate();
+  getCurrentDate(currentMonth, currentYear, currentDay);
+  getCurrentWeek();
+  getCurrentMonthInDays(currentMonth, currentYear);
 });
 
-function getCurrentDate() {
-  const today = new Date();
-  const currentMonth = today.getMonth();
-  const currentYear = today.getFullYear();
-  const currentDay = today.getDate();
-  return `${currentYear}년 ${currentMonth + 1}월 ${currentDay}일`;
+function getCurrentDate(currentMonth, currentYear, currentDay) {
+  const currentDate = document.getElementById("currentDate");
+  currentDate.innerText = `${currentYear}년 ${
+    currentMonth + 1
+  }월 ${currentDay}일`;
 }
 
 function getCurrentWeek() {
   const DAYS = ["일", "월", "화", "수", "목", "금", "토"];
-  return DAYS.forEach((data) => {
+  DAYS.forEach((data) => {
     const p = document.createElement("p");
     p.textContent = data;
     dayOfWeek.appendChild(p);
   });
 }
 
-function getCurrentMonthInDays() {
+function getCurrentMonthInDays(currentMonth, currentYear) {
   const calendarDatas = document.getElementById("calendarDatas");
-  const today = new Date();
-  const currentMonth = today.getMonth();
-  const currentYear = today.getFullYear();
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
 
   for (let i = 1; i <= daysInMonth; i++) {
